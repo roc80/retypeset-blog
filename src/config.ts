5735b48652b137072514695d816b5c6193b926ae
roc <lipeng1080@gmail.com>
2025-09-16 11:05:18 +0800

    deploy: update site url

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const themeConfig: ThemeConfig = {
     // author name
     author: 'Roc',
     // site url
-    url: 'https://roc80.github.io',
+    url: 'https://rocli.cn',
     // base path
     // root directory for all pages and assets
     base: '/', // e.g., '/blog', '/docs'
